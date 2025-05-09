ebe66462ef7108f09c47e77f5374d4fd3c7904d1
taker1974 <kostus.online.1974@yandex.ru>
2025-05-09 12:50:55 +0300

    doc: формирование книги

---
+++ combine.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+filetmp="out/tmp.md"
+combined="out/combined.md"
+title="Вопросы для подготовки"
+book="QA"
+
+# Создаем итоговый файл с заданным заголовком первого уровня
+#echo "---" > $filetmp
+#echo "title: "Вопросы для подготовки"" >> $filetmp
+#echo "author: "Константин Терских, kostus.online.1974@yandex.ru"" >> $filetmp
+#echo "---" >> $filetmp
+#echo "" >> $filetmp
+
+echo "# $title" > $filetmp
+
+# Явно задаем порядок и список файлов
+files=(
+  "Q-JavaCore.md"
+  "Q-OOP.md"
+  "Q-String.md"
+  "Q-Enum.md"
+  "Q-FunctionalInterfaces.md"
+  "Q-StreamAPI.md"
+  "Q-Collections.md"
+)
+
+# Обрабатываем каждый markdown-файл (кроме итогового)
+for file in "${files[@]}"; do
+  # Добавляем разделитель между файлами (теперь - разделами)
+  echo -e "\n" >> $filetmp
+
+  # Понижаем уровень всех заголовков
+  sed -E '
+    s/^(#+) /#\1 /g
+  ' "$file" | sed '/./,$!d' >> $filetmp
+done
+
+# Удаляем лишние пустые строки и т.п.
+cat $filetmp | sed '/^$/{N; /\n$/D}' > $combined
+rm $filetmp
+
+pandoc $combined -T "$title" --metadata title="$title" -o "out/$book.html"
+pandoc $combined -T "$title" --metadata title="$title" -o "out/$book.fb2"
+pandoc $combined -T "$title" --metadata title="$title" -o "out/$book.epub"
